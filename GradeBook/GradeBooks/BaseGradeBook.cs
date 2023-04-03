6d91f8be1230114c156243e0553519e780c03c89
Jakub Nowak <jakub.nowak@microsoft.wsei.edu.pl>
2023-04-03 12:10:55 +0200

    test

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -147,7 +147,7 @@ namespace GradeBook.GradeBooks
                     gpa = 0;
                     break;
             }
-            if (IsWeighted && (studentType == StudentType.Honors || StudentType.DualEnrolled))
+            if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
                 gpa++;
             return gpa;
         }
